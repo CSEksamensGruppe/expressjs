f1d3e51f595d29a4bae3a52ca54ca9131e9f8023
wandriey <jona434g@edu.easj.dk>
2018-04-16 12:56:39 +0200

    Routing til alle speakers eller en enkel

--- app/app.js
+++ app/app.js
@@ -2,41 +2,46 @@ var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json');
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', function(req, res) {
-  res.send(`
-      <h1>Welcome</h1>
-      <p>Roux Academy Meetups put together artists from all walks of life</p>
-  `);
+    res.send(`
+        <h1>Welcome</h1>
+        <p>Roux Academy Meetups put together 
+        artissts form alle</p> 
+        `);
 });
 
+//Her laver vi en anden route, som går til speakers
 app.get('/speakers', function(req, res) {
-  var info = '';
-  dataFile.speakers.forEach(function(item) {
+    var info = '';
+    dataFile.speakers.forEach(function(item){
     info += `
-    <li>
-      <h2>${item.name}</h2>
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
-  res.send(`
-      <h1>Roux Academy Meetups</h1>
-      ${info}
-  `);
+        <li>
+            <h2>${item.name}</h2>
+            <p>${item.summary}</p>
+        </li>
+        `;
+    });
+  res.send(` 
+    <h1> Roux Academy Meetups</h1>
+    ${info}`);
 });
 
+//til en bestemt speaker
 app.get('/speakers/:speakerid', function(req, res) {
-
-  var speaker = dataFile.speakers[req.params.speakerid];
-  res.send(`
-      <h1>${speaker.title}</h1>
-      <h2>with ${speaker.name}</h2>
-      <p>${speaker.summary}</p>
-  `);
+    
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(` 
+        <h1>${speaker.title}</h1>
+        <h2>with ${speaker.name}</h2>
+        <p>${speaker.summary}</p>
+        `);
 });
+ 
+
+
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port ' + app.get('port'));
+  console.log('Listening on port' + app.get('port'));
 });
